fd95100efdbb14ae3337081b9972ad78fed568e1
aminul@atilimited.net <aminul@atilimited.net>
2025-02-12 17:53:24 +0600

    login test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller } from "@nestjs/common";
+import { LoginDto } from "./login.dot";
+import { AuthService } from "./auth.service";
 
-@Controller('auth')
-export class AuthController {}
+@Controller("auth")
+export class AuthController {
+  constructor(public readonly authService: AuthService) {}
+  async login(loginDto: LoginDto) {
+    try {
+      const results = await this.authService.login(loginDto);
+      return results;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
