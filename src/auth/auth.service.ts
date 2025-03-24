3cbd7c96398a44df69d26b3295767166dab62c04
aminul@atilimited.net <aminul@atilimited.net>
2025-03-24 11:00:43 +0600

    user register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Users } from "../entities/users";
 import { Repository } from "typeorm";
 import * as bcrypt from "bcryptjs";
 import { JwtService } from "@nestjs/jwt";
 import { LoginDto } from "./dto/login.dto";
+import { RegisterDto } from "./dto/register.dto";
 @Injectable()
 export class AuthService {
   constructor(
@@ -27,9 +32,39 @@ export class AuthService {
     return null;
   }
 
+  async register(registerDto: RegisterDto) {
+    try {
+      // Check if user already exists
+      const existingUser = await this.userRepository.find({
+        where: {
+          email: registerDto.email,
+        },
+      });
+      if (existingUser) {
+        throw new ConflictException("User with this username already exists");
+      }
+
+      // Hash password
+      const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+      const resume = this.userRepository.create({
+        ...registerDto,
+        password: hashedPassword,
+      });
+      // Save the Resume userRepository
+      const savedResume = await this.userRepository.save(resume);
+      return savedResume;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async login(loginDto: LoginDto) {
     // Mock logic for login
-    if (loginDto.email === "test@example.com" && loginDto.password === "password123") {
+    if (
+      loginDto.email === "test@example.com" &&
+      loginDto.password === "password123"
+    ) {
       return { access_token: "mockAccessToken" };
     }
     throw new UnauthorizedException();
