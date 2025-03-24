369873becf13c121c2166461713a325d9fa0dca8
aminul@atilimited.net <aminul@atilimited.net>
2025-03-24 12:26:41 +0600

    auth modify

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { LoginDto } from "./dto/login.dto";
 import { AuthService } from "./auth.service";
+import { RegisterDto } from "./dto/register.dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(public readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto) {
+    try {
+      const results = await this.authService.register(registerDto);
+      return results;
+    } catch (error) {
+      console.log(error);
+    }
+  }
   async login(loginDto: LoginDto) {
     try {
       const results = await this.authService.login(loginDto);
