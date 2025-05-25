32d0da9d7647e74c80f0484ec10326a02d0b6f86
aminul@atilimited.net <aminul@atilimited.net>
2025-05-25 10:47:27 +0600

    start client processes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppDataSource } from "./data-source";
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource), AuthModule],
+  imports: [TypeOrmModule.forRoot(AppDataSource), AuthModule, ClientsModule],
   controllers: [],
   providers: [],
 })
