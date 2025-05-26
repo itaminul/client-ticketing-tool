1dcda1b0acdb5a7304a5081f7bc91ad5d2779e93
aminul@atilimited.net <aminul@atilimited.net>
2025-05-26 11:39:22 +0600

    get all projects service

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Projects } from "src/entities/projects";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(
+    @InjectRepository(Projects)
+    private readonly projectRepository: Repository<Projects>
+  ) {}
+
+  async getAll(page: number = 1, limit: number = 1) {
+    try {
+      const [result, total] = await this.projectRepository.findAndCount({
+        take: limit,
+        skip: (page - 1) * limit,
+        order: {
+          id: "DESC",
+        },
+        select: {
+          id: true,
+          projectName: true,
+          projectDescripton: true,
+          phone: true,
+        },
+      });
+      return {
+        result: result,
+        total,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+}
