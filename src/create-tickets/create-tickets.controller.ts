ce024e488ed3945cf4f4750569cc0d3ede5db070
aminul@atilimited.net <aminul@atilimited.net>
2025-06-01 16:35:38 +0600

    get all ticket

--- src/create-tickets/create-tickets.controller.ts
+++ src/create-tickets/create-tickets.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query } from "@nestjs/common";
+import { CreateTicketsService } from "./create-tickets.service";
+import { BaseController } from "src/common/base.controller";
+import { GenerateTicket } from "src/entities/generate-tickets";
 
-@Controller('create-tickets')
-export class CreateTicketsController {}
+@Controller("create-tickets")
+export class CreateTicketsController extends BaseController<GenerateTicket> {
+  constructor(public readonly createTicketService: CreateTicketsService) {
+    super(createTicketService);
+  }
+
+  @Get("list")
+  async getAll(
+    @Query("page") page: number = 1,
+    @Query("limit") limit: number = 10
+  ) {
+    const data = await this.createTicketService.getAll(page, limit);
+    return {
+      status: HttpStatus.OK,
+      data: data.result,
+      error: null,
+      pagination: {
+        totalItems: data.total,
+        totalPages: Math.ceil(data.total / limit),
+        currentPage: page,
+        itemsPerPage: limit,
+      },
+    };
+  }
+}
